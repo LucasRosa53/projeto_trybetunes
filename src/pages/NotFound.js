5d4b304bda11286e0e5cac0aaaf0dcc9defeea0c
LucasRosa53 <lucasrosa223@hotmail.com>
2022-12-14 16:31:09 -0300

    componentes criados requisito 1

---
+++ src/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div data-testid="page-not-found"> </div>
+    );
+  }
+}
+export default NotFound;
