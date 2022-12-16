b31bb893aaf7f4852dd55316164e9eda6973f59e
LucasRosa53 <lucasrosa223@hotmail.com>
2022-12-16 17:52:39 -0300

    resolvido requisito 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -7,21 +7,27 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import Header from './pages/Header';
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Route exact path="/" component={ Login } />
-          <Route path="/search" component={ Search } />
-          <Route path="/album/:id" component={ Album } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route path="/profile" component={ Profile } />
-          <Route path="/profile/edit" component={ ProfileEdit } />
-          <NotFound />
-        </div>
-      </BrowserRouter>
+      <div>
+        <BrowserRouter>
+          <Header />
+          <main>
+            <Switch>
+              <Route exact path="/" component={ Login } />
+              <Route path="/search" component={ Search } />
+              <Route path="/album/:id" component={ Album } />
+              <Route path="/favorites" component={ Favorites } />
+              <Route path="/profile" component={ Profile } />
+              <Route path="/profile/edit" component={ ProfileEdit } />
+              <NotFound />
+            </Switch>
+          </main>
+        </BrowserRouter>
+      </div>
     );
   }
 }
--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Header extends Component {
+  state = {
+    name: '',
+    loading: false,
+
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true }, async () => {
+      const userName = await getUser();
+      this.setState({ name: userName });
+      this.setState({ loading: false });
+    });
+  }
+
   render() {
+    const { name, loading } = this.state;
+
     return (
-      <div>
-        <p>lucas</p>
-      </div>
+      <header data-testid="header-component">
+        <nav>
+          <Link to="/search">Procurar</Link>
+          <Link to="/album/:id">Album</Link>
+          <Link to="/favorites">Favoritos</Link>
+          <Link to="/profile">Perfil</Link>
+          <Link to="/profile/edit">Edição de Perfil</Link>
+        </nav>
+        <section data-testid="header-user-name">
+          { loading === true ? <Carregando /> : <p>{ name.name }</p> }
+        </section>
+      </header>
     );
   }
 }
