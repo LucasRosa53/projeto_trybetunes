647853831df1bd4cf86fe2629b3f3577e082ecfb
LucasRosa53 <lucasrosa223@hotmail.com>
2023-01-09 16:57:51 -0300

    resolvido requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  state = {
+    name: '',
+    buttonSubmitDisabled: true,
+  };
+
+  validacao = () => {
+    const { name } = this.state;
+    const maiorQdois = 2;
+    if (name.length >= maiorQdois) {
+      this.setState({
+        buttonSubmitDisabled: false,
+      });
+    }
+  };
+
+  funcTarget = ({ target:
+    { value, name } }) => {
+    this.setState({ [name]: value }, this.validacao);
+  };
+
   render() {
+    const { buttonSubmitDisabled, name } = this.state;
     return (
-      <div data-testid="page-search" />
+      <div data-testid="page-search">
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            name="name"
+            value={ name }
+            onChange={ this.funcTarget }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ buttonSubmitDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
+      </div>
     );
   }
 }
