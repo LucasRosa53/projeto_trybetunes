3ae3f44ed2db6a2886881030a9b7b5f1a857cf19
LucasRosa53 <lucasrosa223@hotmail.com>
2023-01-14 10:51:11 -0300

    resolvido requisito 6 com mentoria do Douglas Santos

---
+++ src/components/AlbumCard.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class AlbumCard extends Component {
+  render() {
+    const { collectionName, collectionId } = this.props;
+    console.log(this.props);
+    return (
+      <li>
+        <Link
+          data-testid={ `link-to-album-${collectionId}` }
+          to={ `/album/${collectionId}` }
+        >
+          { collectionName }
+
+        </Link>
+      </li>
+    );
+  }
+}
+export default AlbumCard;
+
+AlbumCard.propTypes = {
+  collectionName: PropTypes.string.isRequired,
+  collectionId: PropTypes.string.isRequired,
+};
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import AlbumCard from '../components/AlbumCard';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Carregando from './Carregando';
 
 class Search extends Component {
   state = {
     name: '',
     buttonSubmitDisabled: true,
+    loading: false,
+    albums: [],
+    artista: '',
   };
 
-  validacao = () => {
+  funcApi = async (e) => {
+    const { name } = this.state;
+    e.preventDefault();
+    this.setState({ artista: name,
+      loading: true });
+    const albums = await searchAlbumsAPI(name);
+    this.setState({ albums, loading: false, name: '' });
+  };
+
+  validacaoPesquisar = () => {
     const { name } = this.state;
     const maiorQdois = 2;
     if (name.length >= maiorQdois) {
@@ -18,29 +33,57 @@ class Search extends Component {
 
   funcTarget = ({ target:
     { value, name } }) => {
-    this.setState({ [name]: value }, this.validacao);
+    this.setState({ [name]: value }, this.validacaoPesquisar);
   };
 
   render() {
-    const { buttonSubmitDisabled, name } = this.state;
+    const { buttonSubmitDisabled, name, loading, albums, artista } = this.state;
     return (
       <div data-testid="page-search">
         <form>
-          <input
-            data-testid="search-artist-input"
-            type="text"
-            name="name"
-            value={ name }
-            onChange={ this.funcTarget }
-          />
-          <button
-            type="submit"
-            data-testid="search-artist-button"
-            disabled={ buttonSubmitDisabled }
-          >
-            Pesquisar
-          </button>
+          <div>
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              name="name"
+              value={ name }
+              onChange={ this.funcTarget }
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              data-testid="search-artist-button"
+              disabled={ buttonSubmitDisabled }
+              onClick={ this.funcApi }
+            >
+              Pesquisar
+            </button>
+          </div>
         </form>
+        <main>
+          { artista && (
+            <p>
+              Resultado de álbuns de:
+              {' '}
+              { artista }
+              {' '}
+            </p>) }
+          { loading ? <Carregando /> : (
+            <ul>
+              {
+                albums.length === 0 && artista
+                  ? (<p>`Nenhum álbum foi encontrado`</p>) : (
+                    albums.map((album) => (<AlbumCard
+                      { ...album }
+                      key={ album.collectionId }
+                    />))
+                  )
+
+              }
+            </ul>
+          )}
+        </main>
       </div>
     );
   }
