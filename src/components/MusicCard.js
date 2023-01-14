e21e423d3945a7b3a1e44e5a4d879d88f2f27ec2
LucasRosa53 <lucasrosa223@hotmail.com>
2023-01-14 14:13:50 -0300

    resolvido reqsuiti 8

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
+import Carregando from '../pages/Carregando';
+import { addSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends Component {
+  state = {
+    loading: false,
+  };
+
+  funcSong = async () => {
+    const { musica } = this.props;
+    this.setState({ loading: true });
+    await addSong(musica);
+    this.setState({ loading: false });
+  };
+
   render() {
     const { musica } = this.props;
+    const { loading } = this.state;
     return (
       <div>
-        <audio data-testid="audio-component" src={ musica.previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
+        <div>
+          <audio data-testid="audio-component" src={ musica.previewUrl } controls>
+            <track kind="captions" />
+            O seu navegador não suporta o elemento
+            {' '}
+            {' '}
+            <code>audio</code>
+            .
+          </audio>
+          <p>{ musica.trackName }</p>
+        </div>
+        { loading && <Carregando /> }
+        <label
+          htmlFor={ musica.trackId }
+        >
           {' '}
-          {' '}
-          <code>audio</code>
-          .
-        </audio>
-        <p>{ musica.trackName }</p>
+          Favorita
+          <input
+            data-testid={ `checkbox-music-${musica.trackId}` }
+            id={ musica.trackId }
+            type="checkbox"
+            onChange={ this.funcSong }
+          />
+        </label>
       </div>
     );
   }
