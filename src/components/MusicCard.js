3f86d0a1ed084f8d663e53d75676437b23063af6
LucasRosa53 <lucasrosa223@hotmail.com>
2023-01-16 10:21:49 -0300

    resolvido requisito 9 e arrumado requisito 8 ajuda Lucas Augusto e DOuglas Santos

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Carregando from '../pages/Carregando';
-import { addSong } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class MusicCard extends Component {
   state = {
     loading: false,
+    mscFavorita: false,
+  };
+
+  async componentDidMount() {
+    const guardaFunc = await this.funcMusicaFavorita();
+    this.setState({ mscFavorita: guardaFunc });
+  }
+
+  funcMusicaFavorita = async () => {
+    const { musica: { trackId } } = this.props;
+    const musicaFavorita = await getFavoriteSongs();
+    return musicaFavorita.map((e) => e.trackId).includes(trackId);
   };
 
   funcSong = async () => {
     const { musica } = this.props; /* validacao */
+    const { mscFavorita } = this.state;
     this.setState({ loading: true });
     await addSong(musica);
-    this.setState({ loading: false });
+    this.setState({ loading: false, mscFavorita: !mscFavorita });
   };
 
   render() {
     const { musica } = this.props;
-    const { loading } = this.state;
+    const { loading, mscFavorita } = this.state;
     return (
       <div>
         <div>
@@ -41,7 +54,8 @@ class MusicCard extends Component {
             data-testid={ `checkbox-music-${musica.trackId}` }
             id={ musica.trackId }
             type="checkbox"
-            onChange={ this.funcSong }
+            checked={ mscFavorita }
+            onClick={ this.funcSong }
           />
         </label>
       </div>
